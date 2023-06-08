8f66fe5818177e0145686895417a1e0af3c76a2b
joelc0193 <Joelc0193@gmail.com>
2023-06-08 00:55:09 -0400

    changes

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class FirestoreService {
+import 'firestore_interface.dart';
+
+class FirestoreService implements FirestoreInterface {
   final FirebaseFirestore _firestore;
   final FirebaseAuth _auth;
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,60 +1,34 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mockito/mockito.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:st_james_park_app/home_page.dart';
-
-class MockFirestore extends Mock implements FirebaseFirestore {}
-class MockFirebaseAuth extends Mock implements FirebaseAuth {}
-
-class TestableMyApp extends StatelessWidget {
-  final FirebaseInitializer initializer;
-
-  TestableMyApp({required this.initializer});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FutureBuilder(
-        future: initializer.initializeApp(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const MyHomePage();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
-          return const CircularProgressIndicator();
-        },
-      ),
-    );
-  }
-}
+import 'package:st_james_park_app/main.dart';
+import 'package:st_james_park_app/services/firestore_service.dart';
+import 'package:st_james_park_app/services/auth_service.dart';
+import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Create instances of your mock classes
-    MockFirestore mockFirestore = MockFirestore();
-    MockFirebaseAuth mockFirebaseAuth = MockFirebaseAuth();
+  final mockFirestore = MockFirestoreInstance();
+  final mockAuth = MockFirebaseAuth();
+  final firestoreService = FirestoreService(
+    firestore: mockFirestore,
+    auth: mockAuth,
+  );
+  final authService = AuthService(
+    auth: mockAuth,
+  );
 
-    // Use the mock instances in your tests
-    await tester.pumpWidget(MaterialApp(
-      home: MyHomePage(
-        firestore: mockFirestore,
-        auth: mockFirebaseAuth,
-      ),
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp(
+      firestoreService: firestoreService,
+      authService: authService,
     ));
 
-    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
