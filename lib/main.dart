1b8e9e9cb718ad758a3a24451a8845691c8a15f6
joelc0193 <Joelc0193@gmail.com>
2023-06-08 18:50:59 -0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 import './services/firestore_service.dart';
 import 'my_home_page.dart';
@@ -9,7 +10,14 @@ import 'my_home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    Provider<FirebaseFirestore>(
+      create: (_) => FirebaseFirestore.instance,
+    ),
+    Provider<FirebaseAuth>(
+      create: (_) => FirebaseAuth.instance,
+    ),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +25,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final firestore = Provider.of<FirebaseFirestore>(context);
+    final auth = Provider.of<FirebaseAuth>(context);
     return MaterialApp(
       title: 'St James Park',
       theme: ThemeData(
