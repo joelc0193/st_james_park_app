6b8fd4462e8870a631391c9fe19673a8fbece62b
joelc0193 <Joelc0193@gmail.com>
2023-06-08 14:50:42 -0400

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,21 +5,35 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:st_james_park_app/main.dart';
 import 'package:st_james_park_app/mocks.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
     // Create your mock objects
     final MockFirebaseApp mockApp = MockFirebaseApp();
     final MockFirebaseFirestore mockFirestore = MockFirebaseFirestore();
     final MockFirebaseAuth mockAuth = MockFirebaseAuth();
 
-    
-    await tester.pumpWidget(const MyApp());
-
+    // Provide the mock objects using provider
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<FirebaseFirestore>(create: (_) => mockFirestore),
+          Provider<FirebaseAuth>(create: (_) => mockAuth),
+        ],
+        child: MyApp(),
+      ),
+    );
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
