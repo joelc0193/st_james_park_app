7eec1d2cde10d90824e02fb542c5ad336e563d58
joelc0193 <Joelc0193@gmail.com>
2023-06-08 01:29:34 -0400

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,33 +3,56 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:st_james_park_app/main.dart';
 import 'package:st_james_park_app/services/firestore_service.dart';
 import 'package:st_james_park_app/services/auth_service.dart';
-import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 
+import 'package:mockito/mockito.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MockFirestoreService extends Mock implements FirestoreService {
+  
+  @override
+  Future<void> incrementNumber(String path, int number) {
+    // TODO: implement incrementNumber
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DocumentSnapshot> getNumber(String path) {
+    // TODO: implement getNumber
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logIn(String email, String password) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp(String email, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<QuerySnapshot> streamData(String path) {
+    // TODO: implement streamData
+    throw UnimplementedError();
+  }
+}
+
 void main() {
-  final mockFirestore = MockFirestoreInstance();
-  final mockAuth = MockFirebaseAuth();
-  final firestoreService = FirestoreService(
-    firestore: mockFirestore,
-    auth: mockAuth,
-  );
-  final authService = AuthService(
-    auth: mockAuth,
-  );
+  final mockFirestoreService = MockFirestoreService();
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp(
-      firestoreService: firestoreService,
-      authService: authService,
-    ));
+  test('incrementNumber increases the number in Firestore', () async {
+    // Arrange
+    when(mockFirestoreService.incrementNumber('numbers/currentNumber', 1))
+        .thenAnswer((_) async => null);
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Act
+    await mockFirestoreService.incrementNumber('path', 1);
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Assert
+    verify(mockFirestoreService.incrementNumber('path', 1)).called(1);
   });
 }
