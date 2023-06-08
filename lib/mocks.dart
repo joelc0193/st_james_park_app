6e59cedb7f2210f402afb41207948ef356f2c799
joelc0193 <Joelc0193@gmail.com>
2023-06-08 19:50:13 -0400

    changes

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -4,16 +4,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class MockFirebaseApp extends Mock implements FirebaseApp {}
-class MockCollectionReference extends Mock implements CollectionReference {}
+class MockCollectionReference extends Mock implements CollectionReference<Map<String, dynamic>> {}
 
 class MockFirebaseFirestore extends Mock implements FirebaseFirestore {
-  final MockCollectionReference _collectionReference =
-      MockCollectionReference();
+  final MockCollectionReference _collectionReference = MockCollectionReference();
 
   @override
-  CollectionReference collection(String path) {
+  CollectionReference<Map<String, dynamic>> collection(String path) {
     return _collectionReference;
   }
 }
+
 
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
