1d50cdb0fdbbb90ab9c1ad681261080f197e667b
joelc0193 <Joelc0193@gmail.com>
2023-06-08 19:53:57 -0400

    changes

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -4,7 +4,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class MockFirebaseApp extends Mock implements FirebaseApp {}
-class MockCollectionReference extends Mock implements CollectionReference<Map<String, dynamic>> {}
+class MockCollectionReference extends Mock implements CollectionReference<Map<String, dynamic>> {
+  final MockDocumentReference _documentReference = MockDocumentReference();
+
+  @override
+  DocumentReference<Map<String, dynamic>> doc([String? documentPath]) {
+    return _documentReference;
+  }
+}
 
 class MockFirebaseFirestore extends Mock implements FirebaseFirestore {
   final MockCollectionReference _collectionReference = MockCollectionReference();
@@ -12,6 +19,20 @@ class MockFirebaseFirestore extends Mock implements FirebaseFirestore {
   @override
   CollectionReference<Map<String, dynamic>> collection(String path) {
     return _collectionReference;
+  }
+}
+
+class MockDocumentReference extends Mock implements DocumentReference<Map<String, dynamic>> {
+  @override
+  Stream<DocumentSnapshot<Map<String, dynamic>>> snapshots({bool includeMetadataChanges = false}) {
+    return Stream.value(MockDocumentSnapshot());
+  }
+}
+
+class MockDocumentSnapshot extends Mock implements DocumentSnapshot<Map<String, dynamic>> {
+  @override
+  Map<String, dynamic>? data() {
+    return {'number': 1};  // return the mock data you want
   }
 }
 
