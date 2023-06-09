5fd879019309d993b3697606f327f9b44329d22d
joelc0193 <Joelc0193@gmail.com>
2023-06-09 10:26:51 -0400

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:st_james_park_app/main.dart';
-import 'package:st_james_park_app/mocks.dart';
+import 'mocks.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
