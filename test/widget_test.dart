4d8672b5236dce262a4d9c5de5b0e69576b51528
joelc0193 <Joelc0193@gmail.com>
2023-06-11 12:35:39 -0400

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -21,12 +21,51 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_goldens/flutter_goldens.dart';
 
 void main() {
+  late MockFirebaseFirestore mockFirestore;
+  late MockFirebaseAuth mockAuth;
+  late FirestoreService firestoreService;
+
+  setUp(() {
+    mockFirestore = MockFirebaseFirestore();
+    mockAuth = MockFirebaseAuth();
+    firestoreService =
+        FirestoreService(firestore: mockFirestore, auth: mockAuth);
+  });
+
+  test('getNumber returns the current number from Firestore', () async {
+    // Setup: Add a number to Firestore.
+    await mockFirestore
+        .collection('numbers')
+        .doc('currentNumber')
+        .update({'currentNumber': 42});
+
+    // Action: Call getNumber().
+    var numberStream = firestoreService.getNumber();
+
+    // Assert: Check that the number returned by getNumber() is the number we added to Firestore.
+    numberStream.listen(expectAsync1((snapshot) {
+      expect(snapshot.data(), isNotNull);
+      expect((snapshot.data() as Map<String, dynamic>)['currentNumber'], 42);
+    }));
+  });
+
+  // test('signOut signs out the user', () async {
+  //   // Setup: Sign in a user.
+  //   await mockAuth.signInAnonymously();
+
+  //   // Action: Call signOut().
+  //   await firestoreService.signOut();
+
+  //   // Assert: Check that the user is now signed out.
+  //   var user = mockAuth.currentUser;
+  //   expect(user, isNull);
+  // });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     TestWidgetsFlutterBinding.ensureInitialized();
 
     final MockFirebaseFirestore mockFirestore = MockFirebaseFirestore();
     final MockFirebaseAuth mockAuth = MockFirebaseAuth();
-
     // Provide the mock objects using provider
     await tester.pumpWidget(
       MultiProvider(
