4d5253a25ee980e0dd0b4d90b9d4bcfa7b70ebc2
joelc0193 <Joelc0193@gmail.com>
2023-06-16 08:33:36 -0400

    Add text_styles.dart for reusable text styles

---
+++ lib/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static final TextStyle heading = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final TextStyle subheading = TextStyle(
+    fontSize: 18,
+    color: Colors.grey[700],
+  );
+}
